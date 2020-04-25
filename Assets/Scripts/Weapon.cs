0d8f244fb12a6ffd86274cf6979959accb014db8
Lloyd Risper <lloydrisper@me.com>
2020-04-25 06:37:07 -0400

    Adding Raycast To weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera _fpsCamera;
+    [SerializeField] float _range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(_fpsCamera.transform.position, _fpsCamera.transform.forward, out hit, _range);
+    }
+}
