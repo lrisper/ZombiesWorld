4b56657a7c00fcca2ef1c120be0ea0be92880420
Lloyd Risper <lloydrisper@me.com>
2020-04-27 03:50:01 -0400

    Player Input To Select Weapon

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -12,6 +12,62 @@ public class WeaponSwitcher : MonoBehaviour
         SetWeaponActive();
     }
 
+    void Update()
+    {
+        int previousWeapon = _curentWeapon;
+        ProcessKeyInput();
+        ProcessScroolWheel();
+        if (previousWeapon != _curentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            _curentWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            _curentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            _curentWeapon = 2;
+        }
+    }
+
+    // scrool up
+    private void ProcessScroolWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (_curentWeapon >= transform.childCount - 1)
+            {
+                _curentWeapon = 0;
+            }
+            else
+            {
+                _curentWeapon++;
+            }
+        }
+
+        // scrool down
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (_curentWeapon <= 0)
+            {
+                _curentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                _curentWeapon--;
+            }
+        }
+    }
+
     private void SetWeaponActive()
     {
         int weaponIndex = 0;
@@ -29,8 +85,5 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    void Update()
-    {
 
-    }
 }
